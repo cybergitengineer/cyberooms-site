2bf91e407ef5cc300327b39db3c5f9ab325ba6f5
Edgar Pfuma <101349680+cybergitengineer@users.noreply.github.com>
2025-07-08 02:40:23 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f0f2f5;
+    color: #333;
+}
+header {
+    background-color: #004466;
+    color: white;
+    padding: 20px;
+    text-align: center;
+}
+main {
+    padding: 20px;
+}
